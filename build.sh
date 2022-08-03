cd903100cd112289b05d8b00576bd299468c6cf1
Simon Soriano <simon0191@gmail.com>
2022-08-03 12:40:23 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+WORKDIR=$(git rev-parse --show-toplevel)
+rm -rf ${WORKDIR}/gen
+mkdir ${WORKDIR}/gen
+go run main.go
+
+for f in ${WORKDIR}/gen/*.svg; do 
+  echo "--- processing: $f"
+  LC_CTYPE="en_US.UTF-8" convert -background none ${f} "${f}.png"
+done
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"text/template"
+)
+
+type ProgressBar struct {
+	Fill     string
+	Width    int // multiple of 12
+	Progress int
+}
+
+func main() {
+	bars := []ProgressBar{}
+	for i := 0; i <= 10; i++ {
+		bars = append(bars, NewProgressBar(120, i*10))
+	}
+	tplSource, err := os.ReadFile("base.svg.tpl")
+	if err != nil {
+		panic(err)
+	}
+	tpl, err := template.New("svg").Parse(string(tplSource))
+	if err != nil {
+		panic(err)
+	}
+	for idx, bar := range bars {
+		buf := bytes.Buffer{}
+		if err := tpl.Execute(&buf, bar); err != nil {
+			panic(err)
+		}
+		os.WriteFile(fmt.Sprintf("gen/%d.svg", idx*10), buf.Bytes(), os.ModeExclusive|0o660)
+	}
+}
+
+func NewProgressBar(fullWidth int, progress int) ProgressBar {
+	bar := ProgressBar{
+		Width:    progress * (fullWidth - 4) / 100, // substract 4 which is the stroke width
+		Progress: progress,
+	}
+	switch {
+	case progress < 50:
+		bar.Fill = "red"
+	case progress < 80:
+		bar.Fill = "yellow"
+	default:
+		bar.Fill = "#15e315"
+	}
+	return bar
+}
